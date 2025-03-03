9a92603b647b4ad31878967e221ac164b0d723d0
riya-ut <riya.arjunkurup@gmail.com>
2025-03-03 12:20:24 +0530

    Initial commit for DSA repository

---
+++ Que.java
@@ -0,0 +1,25 @@
+class Queue {
+    int[] ar;
+    int front;
+    int rear;
+    int size; // how many elements are there in que
+    int capacity; // what is the maximum size of the que
+
+    Queue(int capacity) {
+        this.capacity = capacity;
+        this.ar = new int[capacity];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+
+    }
+
+}
+
+public class Que {
+
+    public static void main(String[] args) {
+
+    }
+
+}
